5410705c16f7c3b77355a682764a913cc04d6f77
ffilipovicc98 <ffilipovicc98@gmail.com>
2023-09-19 00:19:40 +0200

    fix: Adds template literals and changes let with const

--- script.js
+++ script.js
@@ -23,13 +23,13 @@ const handMinutes = document.querySelector('.hand.minutes');
 const handSeconds = document.querySelector('.hand.seconds');
 
 function getCurrentTime() {
-	let date = new Date();
-	let currentHours = date.getHours();
-	let currentMinutes = date.getMinutes();
-	let currentSeconds = date.getSeconds();
-  let currentMilliseconds = date.getMilliseconds();
+	const date = new Date();
+	const currentHours = date.getHours();
+	const currentMinutes = date.getMinutes();
+	const currentSeconds = date.getSeconds();
+  const currentMilliseconds = date.getMilliseconds();
 
-	handHours.style.animationDelay = '-' + (43200/12 * (currentHours%12) + 43200/12 * currentMinutes/60) + 's';
+	handHours.style.animationDelay = `-${(43200/12 * (currentHours%12) + 43200/12 * currentMinutes/60)}s`;
 	// Since animation-duration is set to 3600s at .hands-box .minutes
 	// We should transform [0, 60] -> [0s, 3600s]
 	// Example 1: handle points on 0 -> `-0s`
@@ -37,8 +37,8 @@ function getCurrentTime() {
 	// Example 3: handle points on 60 -> `-3600s`
 	// So formula is: 3600 / 60 * currentMinutes + correctionForSeconds
 	// correctionForSeconds is number between 0 and 3600 / 60
-	handMinutes.style.animationDelay = '-' + (3600 / 60 * (currentMinutes) + 3600/60*currentSeconds/60) + 's';
-	handSeconds.style.animationDelay = '-' + (1000*currentSeconds + currentMilliseconds) + 'ms';
+	handMinutes.style.animationDelay = `-${(3600 / 60 * (currentMinutes) + 3600/60*currentSeconds/60)}s`;
+	handSeconds.style.animationDelay = `-${(1000*currentSeconds + currentMilliseconds)}ms`;
 }
 
 const dateWindow = document.querySelector('.date-window');
